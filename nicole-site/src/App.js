bb283909a7ca055875ff53e83a82973c8dbc7b2a
Nicole <ngasperini@ussilica.com>
2021-02-21 11:15:07 -0600

    Add react, bootstrap, and component structure

---
+++ nicole-site/src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+         Header in App
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
